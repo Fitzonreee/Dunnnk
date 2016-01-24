00e45b24c0865c6cda4db5e86d05a4c8243d35fa
Kevin Fitzhenry <kevin.fitzhenry@createthenext.com>
2016-01-23 18:07:37 -0800

    Added profile template and API request for user information

--- js/app.js
+++ js/app.js
@@ -9,5 +9,6 @@ app.config(function($routeProvider){
   $routeProvider
   .when("/", {controller: "ShotsListCtrl", templateUrl: "partials/home.html"})
   .when("/shots/:id", {controller: "ShowCtrl", templateUrl: "partials/show.html"})
+  .when("/user/:id", {controller: "ShowCtrl", templateUrl: "partials/profile.html"})
   .otherwise({redirectTo: "/"})
 })
--- js/controllers.js
+++ js/controllers.js
@@ -21,11 +21,16 @@ controllers.controller('ShowCtrl', function($scope, $http, $routeParams) {
 
   $http.jsonp('https://api.dribbble.com/v1/shots/'+ id +'?callback=JSON_CALLBACK&access_token=f40087a1b5e7d5c99a63eb635b7dc264c5af09f011e344a6611a928a3020e581').then(function(data) {
     $scope.shot = data.data;
-    console.log(data);
+    // console.log(data);
   })
 
   $http.jsonp('https://api.dribbble.com/v1/shots/'+ id +'/comments?callback=JSON_CALLBACK&access_token=f40087a1b5e7d5c99a63eb635b7dc264c5af09f011e344a6611a928a3020e581').then(function(comment_data) {
     $scope.comments = comment_data.data;
-    console.log(comment_data);
+    // console.log(comment_data);
+  })
+
+  $http.jsonp('https://api.dribbble.com/v1/users/' + id + '?callback=JSON_CALLBACK&access_token=f40087a1b5e7d5c99a63eb635b7dc264c5af09f011e344a6611a928a3020e581').then(function(data) {
+    $scope.user = data.data;
+    console.log(data);
   })
 });
